895a7396f905a34c59f1fd9a537d524118bbaf00
DCH <dching0121@gmail.com>
2019-12-03 11:06:23 +0800

    ActionBar

--- app/src/main/java/tw/org/iii/framlayout/MainActivity.java
+++ app/src/main/java/tw/org/iii/framlayout/MainActivity.java
@@ -1,12 +1,16 @@
 package tw.org.iii.framlayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private Fragment[] fs = new Fragment[5];
+    private String[] titles = {"page1", "page2", "page3"};
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fs[3] = new P3(); fs[4] = new P4();
 
         viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        initActionBar();
         viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
             @Override
             //製造有拉不過去頁面的效果
@@ -37,10 +44,44 @@ public class MainActivity extends AppCompatActivity {
                 else if (position == 4) {
                     viewPager.setCurrentItem(3);
                 }
+                // 控制actionBar隨著畫面移動
+                else {
+                    actionBar.setSelectedNavigationItem(position-1);
+                }
             }
         });
         //預設postion 1開始
         viewPager.setCurrentItem(1);
+    }
+
+    private void initActionBar() {
+        actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        MyTabListener myTabListener = new MyTabListener();
+        actionBar.addTab(
+                actionBar.newTab().setText("Page1").setTabListener(myTabListener));
+        actionBar.addTab(
+                actionBar.newTab().setText("Page2").setTabListener(myTabListener));
+        actionBar.addTab(
+                actionBar.newTab().setText("Page3").setTabListener(myTabListener));
+    }
+
+    private class MyTabListener implements ActionBar.TabListener {
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            viewPager.setCurrentItem(tab.getPosition()+1);
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
     }
 
     public void gotoP1(View view) {
@@ -70,6 +111,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return fs.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = "";
+            if (position != 0 && position != 4) {
+                title = titles[position-1];
+            }
+            return title;
         }
     }
 }
